b156d90713dfd3e27a46d6d5300919d388ade58d
Kiko <145778771+kiko2004@users.noreply.github.com>
2024-01-10 05:45:27 +0100

    Add files via upload

---
+++ Operacionesbasicas.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class c {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("introduce un precio");
+            double precioInicial = scanner.nextDouble();
+        System.out.println("introduce un porcentaje de descuento");
+            double porcentajeDescuento = scanner.nextDouble();
+            double descuento = precioInicial * (porcentajeDescuento / 100);
+            double precioFinal = precioInicial - descuento;
+
+            System.out.println(precioFinal);
+    }
+}
