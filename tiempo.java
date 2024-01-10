88d470403871784aa86071314b3676ea2ae1663e
Kiko <145778771+kiko2004@users.noreply.github.com>
2024-01-10 05:27:16 +0100

    tiempo.java

--- tiempo.java
+++ tiempo.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
+
 public class tiempo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Introduce un numero de segundos");
         int segundosIniciales = scanner.nextInt();
 
-        int horas = segundosIniciales /3600;
-        int minutos = (segundosIniciales % 3600) /60;
+        int horas = segundosIniciales / 3600;
+        int minutos = (segundosIniciales % 3600) / 60;
         int segundos = segundosIniciales % 60;
 
         System.out.println("son" + horas + "horas" + minutos + "minutos" + segundos + "segundos");
