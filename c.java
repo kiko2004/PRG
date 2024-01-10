a18c1ead4af0268a11c43255677f6fb5c5ef1a3f
Kiko <145778771+kiko2004@users.noreply.github.com>
2024-01-10 05:27:44 +0100

    Operacionesbasicas.java

--- c.java
+++ c.java
@@ -5,12 +5,12 @@ public class c {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("introduce un precio");
-            double precioInicial = scanner.nextDouble();
+        double precioInicial = scanner.nextDouble();
         System.out.println("introduce un porcentaje de descuento");
-            double porcentajeDescuento = scanner.nextDouble();
-            double descuento = precioInicial * (porcentajeDescuento / 100);
-            double precioFinal = precioInicial - descuento;
+        double porcentajeDescuento = scanner.nextDouble();
+        double descuento = precioInicial * (porcentajeDescuento / 100);
+        double precioFinal = precioInicial - descuento;
 
-            System.out.println(precioFinal);
+        System.out.println(precioFinal);
     }
 }
