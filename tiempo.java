07e5001c479a183aee5a6226adb390593acacd1a
Kiko <145778771+kiko2004@users.noreply.github.com>
2024-01-10 05:28:38 +0100

    Tiempo.java

--- tiempo.java
+++ tiempo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class tiempo {
+public class Tiempo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Introduce un numero de segundos");
