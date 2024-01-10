8210db6185830a353bc2ef83b2e891a78a497657
Kiko <145778771+kiko2004@users.noreply.github.com>
2024-01-10 05:23:49 +0100

    Add files via upload

---
+++ tiempo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class tiempo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un numero de segundos");
+        int segundosIniciales = scanner.nextInt();
+
+        int horas = segundosIniciales /3600;
+        int minutos = (segundosIniciales % 3600) /60;
+        int segundos = segundosIniciales % 60;
+
+        System.out.println("son" + horas + "horas" + minutos + "minutos" + segundos + "segundos");
+    }
+}
