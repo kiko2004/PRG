ec866a4633f4171781b96fb122cb19dfd4ce85af
Kiko <145778771+kiko2004@users.noreply.github.com>
2024-02-02 17:00:34 +0100

    Create Tienda.java

---
+++ Tienda.java
@@ -0,0 +1,17 @@
+
+public class Tienda {
+    private Bicicleta[] referencias;
+    private int numBicicletas[];
+private int numReferencias;
+
+    public Tienda(int tamanyoInicial){
+        referencias = new Bicicleta[tamanyoInicial];
+        numReferencias = 0;
+    }
+
+    public Bicicleta comprarReferencia(String referencia) {
+
+
+
+    }
+}
